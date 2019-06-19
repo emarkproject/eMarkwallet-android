01e684f748609b58b9786def284d68212d0befaa
Rumhocker <rumhocker@deutsche-emark.org>
2019-06-20 00:19:37 +0200

    Fix Room - Schema export

--- app/src/main/java/de/eMark/tools/database/AppDatabase.java
+++ app/src/main/java/de/eMark/tools/database/AppDatabase.java
@@ -3,7 +3,7 @@ package de.eMark.tools.database;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
-@Database(entities = {DigiTransaction.class}, version = 6)
+@Database(entities = {DigiTransaction.class}, version = 6, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract TransactionDao transactionDao();
 }
