0ed7d93f59916e34e3a3172e778edbdc5d62aa3d
Rumhocker <rumhocker@deutsche-emark.org>
2019-12-26 01:00:51 +0100

    change database name

--- app/src/main/java/de/eMark/tools/sqlite/BRSQLiteHelper.java
+++ app/src/main/java/de/eMark/tools/sqlite/BRSQLiteHelper.java
@@ -43,7 +43,7 @@ public class BRSQLiteHelper extends SQLiteOpenHelper {
         return instance;
     }
 
-    private static final String DATABASE_NAME = "breadwallet.db";
+    private static final String DATABASE_NAME = "eMarkwallet.db";
     private static final int DATABASE_VERSION = 12;
 
     /**
